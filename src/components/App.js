cfc3c0c86d97d5275bff793c3631b11edf1bd8ce
gooseisdead <gooseisdead@mac.com>
2021-01-19 15:02:02 -0500

    toys rendered

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import Header from "./Header";
 import ToyForm from "./ToyForm";
@@ -6,6 +6,15 @@ import ToyContainer from "./ToyContainer";
 
 function App() {
   const [showForm, setShowForm] = useState(false);
+  const [toys, setToys] = useState([])
+
+  useEffect(() => {
+    fetch(`http://localhost:3001/toys`)
+    .then((resp) => resp.json())
+    .then((toyArray) => {
+      setToys(toyArray)
+    })
+  }, [])
 
   function handleClick() {
     setShowForm((showForm) => !showForm);
@@ -18,7 +27,7 @@ function App() {
       <div className="buttonContainer">
         <button onClick={handleClick}>Add a Toy</button>
       </div>
-      <ToyContainer />
+      <ToyContainer api={toys} />
     </>
   );
 }
--- src/components/ToyContainer.js
+++ src/components/ToyContainer.js
@@ -1,9 +1,18 @@
 import React from "react";
 import ToyCard from "./ToyCard";
 
-function ToyContainer() {
+function ToyContainer({ api }) {
+
+  const renderToys = api.map((toy) => 
+    <ToyCard 
+      key={toy.id}
+      name={toy.name}
+      image={toy.image}
+      likes={toy.likes}
+    /> )
+
   return (
-    <div id="toy-collection">{/* Render the collection of ToyCards */}</div>
+    <div id="toy-collection">{renderToys}</div>
   );
 }
 
